6fd869053fcc90e8d5afb225a814c81b8d7cae8b
16mm <114557943+PsychofankakrolaJuliana@users.noreply.github.com>
2022-10-31 17:54:51 +0000

    dgfv

--- KartaPracy1.cpp
+++ KartaPracy1.cpp
@@ -24,5 +24,33 @@ int main()
   // cout << (a-b)*(a-b)*(a-b);
 
   // Zad.4
+  // int a;
+  // int b;
+  // int c;
+  // cin >> a;
+  // cin >> b;
+  // cin >> c;
+  // cout << a*b*c;
+
+  // Zad.5
+  // int a;
+  // int b;
+  // cin >> a;
+  // cin >> b;
+  // cout << (a+b)*0.4;
+
+  // Zad.6
+  // int cena;
+  // cin >> cena;
+  // int cn = cena - cena*0.23;
+  // cout>> cn;
+
+  // Zad.7
+  // int a;
+  // int b;
+  // cin >> a;
+  // cin >> b;
+  // cout << a%b ;
+  
   return 0;
 }
