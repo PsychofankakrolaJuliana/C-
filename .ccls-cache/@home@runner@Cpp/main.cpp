174a5be48c617df2a65e1cb5562712b11709e0b2
16mm <114557943+PsychofankakrolaJuliana@users.noreply.github.com>
2022-10-31 17:31:39 +0000

    ouhoi

---
+++ .ccls-cache/@home@runner@Cpp/main.cpp
@@ -0,0 +1,2 @@
+/home/runner/boilerplate-page-view-time-series-visualizer/time_series_visualizer.cpp import matplotlib.pyplot ModuleNotFoundError: No module named ‘matplotlib’
+import time_series_visualizer
---
+++ KartaPracy1.cpp
@@ -0,0 +1,13 @@
+#include<iostream> 
+using namespace std;
+int main(){
+  // Zad.1
+  int a;
+  int b;
+  cin >> a;
+  cin >> b;
+  cout << a*a+b*b;
+  return 0;
+  //Zad.2
+  
+}
