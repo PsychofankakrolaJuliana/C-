876b194aebbb278c8db776339efbd0bd7e0c23fa
16mm <114557943+PsychofankakrolaJuliana@users.noreply.github.com>
2022-10-31 17:47:00 +0000

    dagvdj

--- KartaPracy1.cpp
+++ KartaPracy1.cpp
@@ -1,13 +1,28 @@
-#include<iostream> 
+#include <iostream> 
 using namespace std;
-int main(){
+int main()
+{
   // Zad.1
-  int a;
-  int b;
-  cin >> a;
-  cin >> b;
-  cout << a*a+b*b;
-  return 0;
-  //Zad.2
+  // int a;
+  // int b;
+  // cin >> a;
+  // cin >> b;
+  // cout << a*a+b*b;
   
+  //Zad.2
+  // int a;
+  // int b;
+  // cin >> a;
+  // cin >> b;
+  // cout << (a+b)*(a+b);
+
+  // Zad.3
+  // int a;
+  // int b;
+  // cin >> a;
+  // cin >> b;
+  // cout << (a-b)*(a-b)*(a-b);
+
+  // Zad.4
+  return 0;
 }
