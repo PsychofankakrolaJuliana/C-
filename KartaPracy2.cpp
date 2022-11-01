94ff344b8d46901ebdd44ba660fdadc9f51f9661
16mm <114557943+PsychofankakrolaJuliana@users.noreply.github.com>
2022-11-01 15:17:00 +0000

    ;kjvgb

---
+++ KartaPracy2.cpp
@@ -0,0 +1,44 @@
+#include<iostream>
+using namespace std;
+int main(){
+  // Zad.1
+  // int a;
+  // cin >> a;
+  // if (a%3==0){
+  //   cout << "Podana liczba dzilei sie przez 3";
+  // }
+  // else {
+  //   cout << "Podana liczba nie jest podzielna przez 3";
+  // }
+  // Zad.2
+  // int a;
+  // cin >> a;
+  // if (99<a<1000 || a%17==0){
+  //   cout << "Podana liczba spelnia oba warunki";
+  // }
+  // else{
+  //   cout << "POdana liczba nie spelnia rzadnego warunku";
+  // }
+  // Zad.3
+  // int wiek;
+  // cin >> wiek;
+  // if (wiek>17){
+  //   cout << "JEstes pelnoletni";
+  // }
+  // else{
+  //   cout << "Nie jestes pelnoletni";
+  // }
+  // Zad.4
+  // int limit=20;
+  // int waga;
+  // cin >> waga;
+  // if(waga<21){
+  //   cout << "Pojazd o podanej wadze moze wjechac na most";
+  // }
+  // else{
+  //   cout << "Pojazd o podanej wadze niemoze wjechac na most";
+  // }
+  // Zad.5
+  
+  return 0
+}
