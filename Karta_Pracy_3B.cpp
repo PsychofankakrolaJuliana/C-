3a71d44201d194da251baa86e989c4e233a65549
15mm <114557943+Qniara@users.noreply.github.com>
2022-12-25 07:56:44 +0100

    Update Karta_Pracy_3B.cpp

--- Karta_Pracy_3B.cpp
+++ Karta_Pracy_3B.cpp
@@ -3,11 +3,31 @@ using namespace std;
 int main()
 {
 	//Zad.1
-	int i = 0;
-	while (i != 0) {
-		i++;
-		i = i ^ 3 + 3;
-		cout << i;
-
-	}
+// 	int i = 0;
+// 	while (i != 0) {
+// 		i++;
+// 		i = i ^ 3 + 3;
+// 		cout << i;
+// 	}
+	//Zad.2
+	//for (int i = 100; i < 1000; i++) {
+	//	if (i % 15 == 0) {
+	//		cout << " " << i;
+	//	}
+	//}
+	//Zad.3
+	//int n;
+	//cout << "Podaj n: ";
+	//cin >> n;
+	//for (int i = 1; i <= n; i++) {
+	//	if (n % i == 0) {
+	//		cout << " " << i;
+	//	}
+	//}
+	//Zad.4
+	//int suma = 0;
+	//for (int i = 10; i < 100; i++) {
+	//	suma = suma + i;
+	//}
+	//cout << "Suma wszystkich liczb dwucyfrowych to: " << suma;
 }
