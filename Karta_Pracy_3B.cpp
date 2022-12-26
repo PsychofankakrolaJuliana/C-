cf324cbdc81291c64753ab919982820378d10a82
15mm <114557943+Qniara@users.noreply.github.com>
2022-12-26 07:45:54 +0100

    Create Karta_Pracy_3B.cpp

---
+++ Karta_Pracy_3B.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    //Zad.1
+    //for (int i = 1; i < 31; i++) {
+    //    cout << " " << i;
+    //}
+    //Zad.2
+    //for (int i = 1; i < 10; i = i + 2) {
+    //    cout << " " << i * i;
+    //}
+    //Zad.3
+    //for (int i = 1000; i < 10000; i++) {
+    //    if (i % 379 == 0) {
+    //        cout << " " << i;
+    //    }
+    //}
+    //Zad.4
+    //for (int i = 100; i < 1000; i++) {
+    //    if (i % 5 == 0 && i % 6 == 0 or i % 11 == 0) {
+    //        cout << " " << i;
+    //    }
+    //}
+    //Zad.5
+    //int n;
+    //int suma = 0;
+    //int x;
+    //cout << "Podaj ile liczb chcesz podac: ";
+    //cin >> n;
+    //for (int i = 0; i < n; i++) {
+    //    cout << "Podaj liczbe: ";
+    //    cin >> x;
+    //    suma = suma + x;
+    //}
+    //cout << "Suma podanych liczb to: " << suma;
+}
