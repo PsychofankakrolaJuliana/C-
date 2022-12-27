ba0769e7cd61d755dbd4f6d4abc3ce6897ecc0d5
15mm <114557943+Qniara@users.noreply.github.com>
2022-12-27 08:03:45 +0100

    Create Karta_Pracy_6A.cpp

---
+++ Karty Pracy cpp/Karta_Pracy_6A.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    //Zad.1 - do poprawy
+    //int suma = 0;
+    //int n;
+    //int x = 0;
+    //cout << "Podaj liczbe: ";
+    //cin >> n;
+    //for (int i = 0; i < n; i++) {
+    //    x = n % 10;
+    //    suma = suma + x;
+    //}
+    //cout << "Suma cyfr podanej liczby jest rowna: " << suma;
+    //Zad.2
+    //int a;
+    //cout << "Podaj liczbe: ";
+    //cin >> a;
+    //int flaga = true;
+    //for (int i = 2; i < a; i++) {
+    //    if (a % i == 0) {
+    //        flaga = false;
+    //    }
+    //}
+    //if (flaga == true) {
+    //    cout << "Podana liczba jest pierwsza";
+    //}
+    //else if(flaga==false) {
+    //    cout << "Podana nie jest pierwsza";
+    //}
+}
