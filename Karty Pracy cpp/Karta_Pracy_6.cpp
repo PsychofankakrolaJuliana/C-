d20ee0396679c0b267436489e0cba0d79f88f800
15mm <114557943+Qniara@users.noreply.github.com>
2022-12-30 07:58:01 +0100

    Create Karta_Pracy_6.cpp

---
+++ Karty Pracy cpp/Karta_Pracy_6.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    //Zad.2
+    //int suma = 0;
+    //for (int i = 100; i < 1000; i++) {
+    //    if (i % 8 == 0 && i % 16 != 0) {
+    //        suma = suma + i;
+    //    }
+    //}
+    //cout << suma;
+    //Zad.3
+    //int ilo = 0;
+    //int x = 0;
+    //for (int i = 10; i < 100; i++) {
+    //    if (i % 7 == 0) {
+    //        x = i;
+    //    }
+    //}
+    //for (int j = 1000; j < 10000; j++) {
+    //    if (j % x == 0) {
+    //        ilo = ilo + 1;
+    //    }
+    //}
+    //cout << ilo;
+}
