304b230baa79eaad8facafae22a8377e20bc6191
15mm <114557943+Qniara@users.noreply.github.com>
2023-02-16 19:34:25 +0100

    Update Karta_Pracy_4.cpp

--- Karty Pracy cpp/Karta_Pracy_4.cpp
+++ Karty Pracy cpp/Karta_Pracy_4.cpp
@@ -3,4 +3,15 @@ using namespace std;
 int main()
 {
     //Zad.1
+    // suma kwadratow liczb
+    int a, b;
+    cout << "Podaj a: ";
+    cin >> a;
+    cout << "Podaj b: ";
+    cin >> b;
+    float suma(float x, float y)
+    {
+        return x * x + y * y;
+    }
+    suma(a, b);
 }
