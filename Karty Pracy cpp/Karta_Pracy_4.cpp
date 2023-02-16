20f07c2b7ee1bf5bdad474a2f9a13b28f22dc5c2
15mm <114557943+Qniara@users.noreply.github.com>
2023-02-16 19:35:02 +0100

    Update Karta_Pracy_4.cpp

--- Karty Pracy cpp/Karta_Pracy_4.cpp
+++ Karty Pracy cpp/Karta_Pracy_4.cpp
@@ -4,14 +4,14 @@ int main()
 {
     //Zad.1
     // suma kwadratow liczb
-    int a, b;
-    cout << "Podaj a: ";
-    cin >> a;
-    cout << "Podaj b: ";
-    cin >> b;
-    float suma(float x, float y)
-    {
-        return x * x + y * y;
-    }
-    suma(a, b);
-}
+    // suma kwadratow liczb
+    //int a, b;
+    //cout << "Podaj a: ";
+    //cin >> a;
+    //cout << "Podaj b: ";
+    //cin >> b;
+    //float suma(float x, float y)
+    //{
+    //    return x * x + y * y;
+    //}
+    //suma(a, b);
