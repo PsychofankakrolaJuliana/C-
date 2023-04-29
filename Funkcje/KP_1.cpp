aa763c18fbc2748f11e54f02a379e287117847ca
Ziemniak <114557943+Qniara@users.noreply.github.com>
2023-04-29 13:02:29 +0200

    Update KP_1.cpp

---
+++ Funkcje/KP_1.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+//Zad.1
+//int f1(int a, int b) {
+//    return a * a + b * b;
+//}
+int main()
+{
+    int x, y;
+    cout << "Podaj liczbe: ";
+    cin >> x;
+    cout << "Podaj liczbe: ";
+    cin >> y;
+    //Wywołania funkcji
+    //1
+    //cout << f(x, y);
+    //2
+}
