1308a7794421a47e7509410933f2469cafc28175
Ziemniak <114557943+Qniara@users.noreply.github.com>
2023-04-30 18:13:20 +0200

    Update KP_2.cpp

--- Funkcje/KP_2.cpp
+++ Funkcje/KP_2.cpp
@@ -1,10 +1,21 @@
 #include <iostream>
 using namespace std;
 //Zad.1
-
+//bool f1(int n) {
+//    if (n % 3 == 0) {
+//        return true;
+//    }
+//    else{
+//        return false;
+//    }
+//}
 
 int main()
 {
+    int x;
+    cout << "Podaj liczbe: ";
+    cin >> x;
     //Odwołania do funkcji
     //1
+    //cout << f1(x);
 }
