e8b8125ff3083e7c521a5200653f0d79879112d2
Ziemniak <114557943+Qniara@users.noreply.github.com>
2023-05-01 10:07:30 +0200

    Update KP_2a.cpp

--- Funkcje/KP_2a.cpp
+++ Funkcje/KP_2a.cpp
@@ -1,1 +1,29 @@
-
+#include <iostream>
+#include <math.h>
+using namespace std;
+int main()
+{
+    int a, b, c, d;
+    cout << "Podaj liczbe: ";
+    cin >> a;
+    cout << "Podaj liczbe: ";
+    cin >> b;
+    //cout << "Podaj liczbe: ";
+    //cin >> c;
+    //cout << "Podaj liczbe: ";
+    //cin >> d;
+    
+    //Odwołania do funkcji
+    //1
+    //cout << f(a, b);
+    //2
+    //cout << f(a, b);
+    //3
+    //cout << f(a, b);
+    //4
+    //cout << f(a, b);
+    //5
+    //cout << f(a, b);
+    //6
+    //cout << f(a, b);
+}
