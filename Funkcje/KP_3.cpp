803a7957c6911cc94e24011dfa403cec2394105a
Ziemniak <114557943+Qniara@users.noreply.github.com>
2023-05-02 08:10:13 +0200

    Update KP_3.cpp

--- Funkcje/KP_3.cpp
+++ Funkcje/KP_3.cpp
@@ -1,1 +1,27 @@
+#include <iostream>
+using namespace std;
 
+//Zad.1
+
+int main()
+{
+    //int x, y;
+    //cout << "Podaj liczbe: ";
+    //cin >> x;
+    //cout << "Podaj liczbe: ";
+    //cin >> y;
+
+    //Odwołania do funkcji
+    // 1
+    //cout << f1();
+    // 2
+    //cout << f2();
+    // 3
+    //cout << f3();
+    // 4
+    //cout << f4();
+    // 5
+    //cout << f5();
+    // 6
+    //cout << f6();
+}
