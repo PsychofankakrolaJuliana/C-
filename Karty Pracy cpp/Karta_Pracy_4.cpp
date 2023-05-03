2774a9bc7046f1636126481f1d59a81db9b5b895
Ziemniak <114557943+Qniara@users.noreply.github.com>
2023-05-03 09:09:08 +0200

    Update Karta_Pracy_4.cpp

--- Karty Pracy cpp/Karta_Pracy_4.cpp
+++ Karty Pracy cpp/Karta_Pracy_4.cpp
@@ -1,26 +1,54 @@
 #include <iostream>
 using namespace std;
 
+//Zad.1
+//suma kwadratow liczb
+//int f11(int x, int y) {
+//    return x * x + y + y;
+//}
+
+//suma cyr w liczbie
+
+//Zad.2
+//int f2(int x) {
+//    int suma = 0;
+//    int temp = 0;
+//    while (x > 0) {
+//        temp = x % 10;
+//        suma = suma + temp;
+//        x = x / 10;
+//    }
+//    return suma;
+//}
+
 //Zad.3
-int f1(int n) {
-    if (n == 0) {
-        return 1;
-    }
-}
+//int f2(int n) {
+//    if (n == 0) {
+//        return 1;
+//        cout << " ";
+//    }
+//    else {
+//        return n * (f2(n - 1));
+//        cout << " ";
+//    }
+//}
 
 int main()
 {
-    int x;
+    int x, y;
     cout << "Podaj liczbe: ";
     cin >> x;
+    //cout << "Podaj liczbe: ";
+    //cin >> y;
     
     //Odwołania do funkcji
     //1
-    
+    //cout << f11(x, y);
+    //cout << f12(x);
     //2
-    
+    //cout << f2(x);
     //3
-    cout << f1(x);
+    //cout << f2(x);
     //4
     //5
     //6
