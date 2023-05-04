332919925b59ffeaa42139f6c7e0f1dbcfe4edb4
Ziemniak <114557943+Qniara@users.noreply.github.com>
2023-05-04 08:34:50 +0200

    Update KP_3B.cpp

--- Funkcje/KP_3B.cpp
+++ Funkcje/KP_3B.cpp
@@ -1,1 +1,22 @@
-XD
+#include <iostream>
+using namespace std;
+
+//Zad.1
+//void f1() {
+//    for (int i = 1; i < 31; i++) {
+//        cout << i << " ";
+//    }
+//}
+
+int main()
+{
+    //int x, y;
+    //cout << "Podaj liczbe: ";
+    //cin >> x;
+    //cout << "Podaj liczbe: ";
+    //cin >> y;
+
+    //Odwołania do funkcji
+    //1
+    //f1();
+}
