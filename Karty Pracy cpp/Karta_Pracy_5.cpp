02b9d8056f4e0e1965dd72d43a6859162adb6104
Ziemniak <114557943+Qniara@users.noreply.github.com>
2023-05-09 09:35:52 +0200

    Update Karta_Pracy_5.cpp

--- Karty Pracy cpp/Karta_Pracy_5.cpp
+++ Karty Pracy cpp/Karta_Pracy_5.cpp
@@ -1,1 +1,40 @@
+#include <iostream>
+#include <math.h>
+using namespace std;
 
+//Zad.1
+bool czypie(int n) {
+    for (int i = 2; i < n; i++) {
+        if (n % i == 0) {
+            return false;
+        }
+        else {
+            return true;
+        }
+    }
+}
+void f1() {
+
+}
+
+int main()
+{
+    int x, y;
+    //cout << "Podaj liczbe: ";
+    //cin >> x;
+    //cout << "Podaj liczbe: ";
+    //cin >> y;
+
+    //Odwołania do funkcji
+    //1
+    f1();
+    //2
+    
+    //3
+
+    //4
+
+    //5
+
+    //6
+}
