d0bee5a3332c8d25da630b257e227cd57060be50
Ziemniak <114557943+Qniara@users.noreply.github.com>
2023-05-11 20:58:04 +0200

    Update KP_6.cpp

--- Funkcje/KP_6.cpp
+++ Funkcje/KP_6.cpp
@@ -1,1 +1,18 @@
+#include <iostream>
+using namespace std;
 
+//Zad.1
+//void f2() {
+//    for (int i = 100; i < 1000; i++) {
+//        if (i % 8 == 0 and i % 16 != 0) {
+//            cout << i << " ";
+//        }
+//    }
+//}
+
+int main()
+{
+    //Odwołania
+    //1
+    //f2();
+}
