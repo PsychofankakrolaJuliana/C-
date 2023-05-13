f7793e97c75b857d5c90d14765a7120c952936c5
Ziemniak <114557943+Qniara@users.noreply.github.com>
2023-05-13 07:43:54 +0200

    Update KP_6.cpp

--- Funkcje/KP_6.cpp
+++ Funkcje/KP_6.cpp
@@ -1,7 +1,28 @@
 #include <iostream>
 using namespace std;
-
 //Zad.1
+//void f1(int a, int b, int c) {
+//    if (c - b == b - a or a - b == b - c) {
+//        cout << "Ciag jest arytmetyczny\n";
+//    }
+//    else if (b / a == c / b or a / b == b / c) {
+//        cout << "Ciag jest gometryczny\n";
+//    }
+//    else {
+//        cout << "To nie jest ciag.\n";
+//    }
+//    if (a < b < c) {
+//        cout << "Ciag jest rosnacy\n";
+//    }
+//    else if (a > b > c) {
+//        cout << "Ciag jest malejacy\n";
+//    }
+//    else {
+//        cout << "Ten ciag jest nijakim debilem\n";
+//    }
+//}
+
+//Zad.2
 //void f2() {
 //    for (int i = 100; i < 1000; i++) {
 //        if (i % 8 == 0 and i % 16 != 0) {
@@ -12,7 +33,17 @@ using namespace std;
 
 int main()
 {
+    //int x, y, z;
+    //cout << "Podaj liczbe: ";
+    //cin >> x;
+    //cout << "Podaj liczbe: ";
+    //cin >> y;
+    //cout << "Podaj liczbe: ";
+    //cin >> z;
+
     //Odwołania
     //1
+    //f1(x, y, x);
+    //2
     //f2();
 }
