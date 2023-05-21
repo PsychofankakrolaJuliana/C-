1ca67161e86559d9ce1d2528667ab0aa3be331ca
Ziemniak <114557943+Qniara@users.noreply.github.com>
2023-05-21 09:12:42 +0200

    Update KP_6A.cpp

--- Funkcje/KP_6A.cpp
+++ Funkcje/KP_6A.cpp
@@ -11,6 +11,22 @@ using namespace std;
 //    return suma;
 //}
 
+//Zad.2
+//bool f2(int n) {
+//    int f = 1;
+//    for (int i = 2; i < n; i++) {
+//        if (n % i == 0) {
+//            f = 0;
+//        }
+//    }
+//    if (f == 0) {
+//        return false;
+//    }
+//    else {
+//        return true;
+//    }
+//}
+
 int main()
 {
     int x, y;
@@ -20,5 +36,8 @@ int main()
     //cin >> y;,
     
     //Odwołania
+    //1
     //cout << f1(x);
+    //2
+    //cout << f2(x);
 }
