c1cab109dc1099f6df8f9ae8941c67769769817e
Ziemniak <114557943+Qniara@users.noreply.github.com>
2023-05-30 11:33:08 +0200

    Update Karta_Pracy_7.cpp

--- Karty Pracy cpp/Karta_Pracy_7.cpp
+++ Karty Pracy cpp/Karta_Pracy_7.cpp
@@ -1,8 +1,27 @@
 #include <iostream>
 #include <cstdlib>
 #include <ctime>
-
 using namespace std;
-int main() {
 
+int main() {
+    const int l = 40;
+    int T[l];
+    srand(time(0));
+    for (int i = 0; i < l; i++) {
+        T[i] = rand() % 90 + 10;
+    }
+    for (int i = 0; i < l; i++) {
+        cout << T[i] << " ";
+    }
+
+    //Zad.1
+    //int maks = 0;
+    //for (int i = 0; i < l; i++) {
+    //    if (T[i] > maks) {
+    //        maks = T[i];
+    //    }
+    //}
+    //cout << maks;
+
+    return 0;
 }
