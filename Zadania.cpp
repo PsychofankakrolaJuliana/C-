fc00789c29ba2eb649695a06e90f4770593ba86f
Ziemniak <114557943+Qniara@users.noreply.github.com>
2023-06-18 07:21:54 +0200

    Create Zadania.cpp

---
+++ Zadania.cpp
@@ -0,0 +1,14 @@
+//Zad.1 
+//Napisz program, który wczytuje liczbę całkowitą od użytkownika i wyświetla jej kwadrat.
+
+//Zad.2
+//Stwórz program, który wczytuje długość boku kwadratu od użytkownika i oblicza jego pole i obwód.
+
+//Zad.3
+//Napisz program, który sprawdza, czy podana liczba jest parzysta czy nieparzysta.
+
+//Zad.4
+//Stwórz prosty kalkulator, który wczytuje dwie liczby od użytkownika i wykonuje na nich podstawowe operacje arytmetyczne (dodawanie, odejmowanie, mnożenie, dzielenie).
+
+//Zad.5
+//Napisz program, który generuje losową liczbę od 1 do 100 i prosi użytkownika o zgadnięcie tej liczby. Program powinien udzielać wskazówek, czy podana liczba jest za duża, za mała lub równa szukanej liczbie.
