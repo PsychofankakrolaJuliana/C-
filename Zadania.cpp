3866968deacb1c4123a7e6f6cf170f63c07a9eaa
Ziemniak <114557943+Qniara@users.noreply.github.com>
2023-06-18 07:29:15 +0200

    Update Zadania.cpp

--- Zadania.cpp
+++ Zadania.cpp
@@ -1,3 +1,7 @@
+#include <iostream>
+using namespace std;
+int main()
+{
 //Zad.1 
 //Napisz program, który wczytuje liczbę całkowitą od użytkownika i wyświetla jej kwadrat.
 
@@ -8,7 +12,10 @@
 //Napisz program, który sprawdza, czy podana liczba jest parzysta czy nieparzysta.
 
 //Zad.4
-//Stwórz prosty kalkulator, który wczytuje dwie liczby od użytkownika i wykonuje na nich podstawowe operacje arytmetyczne (dodawanie, odejmowanie, mnożenie, dzielenie).
+//Stwórz prosty kalkulator, który wczytuje dwie liczby od użytkownika i wykonuje na nich podstawowe operacje 
+//arytmetyczne (dodawanie, odejmowanie, mnożenie, dzielenie).
 
 //Zad.5
-//Napisz program, który generuje losową liczbę od 1 do 100 i prosi użytkownika o zgadnięcie tej liczby. Program powinien udzielać wskazówek, czy podana liczba jest za duża, za mała lub równa szukanej liczbie.
+//Napisz program, który generuje losową liczbę od 1 do 100 i prosi użytkownika o zgadnięcie tej liczby. Program 
+//powinien udzielać wskazówek, czy podana liczba jest za duża, za mała lub równa szukanej liczbie.
+}
