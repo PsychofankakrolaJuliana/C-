226c8fbe8a9beececd0d530451c5750ad91677aa
Ziemniak <114557943+Qniara@users.noreply.github.com>
2023-06-18 07:31:38 +0200

    Update Zadania.cpp

--- Zadania.cpp
+++ Zadania.cpp
@@ -2,20 +2,25 @@
 using namespace std;
 int main()
 {
-//Zad.1 
-//Napisz program, który wczytuje liczbę całkowitą od użytkownika i wyświetla jej kwadrat.
+	//Zad.1 
+	//Napisz program, który wczytuje liczbę całkowitą od użytkownika i wyświetla jej kwadrat.
+	//double n;
+	//cout << "Podaj liczbe: ";
+	//cin >> n;
+	//n = pow(n, 2);
+	//cout << n;
 
-//Zad.2
-//Stwórz program, który wczytuje długość boku kwadratu od użytkownika i oblicza jego pole i obwód.
+	//Zad.2
+	//Stwórz program, który wczytuje długość boku kwadratu od użytkownika i oblicza jego pole i obwód.
 
-//Zad.3
-//Napisz program, który sprawdza, czy podana liczba jest parzysta czy nieparzysta.
+	//Zad.3
+	//Napisz program, który sprawdza, czy podana liczba jest parzysta czy nieparzysta.
 
-//Zad.4
-//Stwórz prosty kalkulator, który wczytuje dwie liczby od użytkownika i wykonuje na nich podstawowe operacje 
-//arytmetyczne (dodawanie, odejmowanie, mnożenie, dzielenie).
+	//Zad.4
+	//Stwórz prosty kalkulator, który wczytuje dwie liczby od użytkownika i wykonuje na nich podstawowe operacje 
+	//arytmetyczne (dodawanie, odejmowanie, mnożenie, dzielenie).
 
-//Zad.5
-//Napisz program, który generuje losową liczbę od 1 do 100 i prosi użytkownika o zgadnięcie tej liczby. Program 
-//powinien udzielać wskazówek, czy podana liczba jest za duża, za mała lub równa szukanej liczbie.
+	//Zad.5
+	//Napisz program, który generuje losową liczbę od 1 do 100 i prosi użytkownika o zgadnięcie tej liczby. Program 
+	//powinien udzielać wskazówek, czy podana liczba jest za duża, za mała lub równa szukanej liczbie.
 }
