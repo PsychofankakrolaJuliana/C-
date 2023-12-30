14c44a931df6d74665fd1c7bda264d0ec5d8a0f9
Marta Mendel <114557943+Qniara@users.noreply.github.com>
2023-12-30 18:01:36 +0100

    Delete main.cpp

--- main.cpp
+++
@@ -1,2 +0,0 @@
-/home/runner/boilerplate-page-view-time-series-visualizer/time_series_visualizer.cpp import matplotlib.pyplot ModuleNotFoundError: No module named ‘matplotlib’
-import time_series_visualizer
