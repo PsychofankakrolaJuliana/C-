bfb5116e2ebf623f8d19fdf35d147de4755f9a4e
Marta Mendel <114557943+Qniara@users.noreply.github.com>
2024-01-15 17:09:02 +0100

    Update KP_6_2.cpp

--- Karty Pracy cpp/KP_6_2.cpp
+++ Karty Pracy cpp/KP_6_2.cpp
@@ -2,5 +2,45 @@
 using namespace std;
 int main()
 {
-    
+    //Zad.4
+    //int ilo = 0;
+    //for (int i = 10; i < 100; i++) if (i % 100 >= (i % 10) * 2) ilo++;
+    //cout << ilo;
+
+    //Zad.5
+    //int suma = 0;
+    //int ilo = 0;
+    //for (int i = 100; i < 1000; i++) {
+    //    if ((i % 1000) > (i % 100) + (i % 10)) {
+    //        suma += i; 
+    //        ilo ++ ;
+    //    }
+    //}
+    //cout << suma << " " << ilo;
+
+    //Zad.6
+    //int suma = 0;
+    //int n;
+    //cout << "Podaj n: ";
+    //cin >> n;
+    //for (int i = 19; i < 19+19*n; i+=19) {
+    //    suma += i;
+    //}
+    //cout << suma;
+
+    //Zad.7
+    //int suma = 0;
+    //int n;
+    //cout << "Podaj n: ";
+    //cin >> n;
+    //for (int i = 999; i >= 100; i--) {
+    //    if (i % 37 == 0) {
+    //        suma = suma + i;
+    //        n = n - i;
+    //    }
+    //    if (n == 0) {
+    //        break;
+    //    }
+    //}
+    //cout << suma;
 }
