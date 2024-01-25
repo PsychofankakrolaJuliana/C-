68039869c504bb612d238131c32c5e6d1ab640f5
Marta Mendel <114557943+Qniara@users.noreply.github.com>
2024-01-25 18:25:20 +0100

    Update KP_6_2.cpp

--- Karty Pracy cpp/KP_6_2.cpp
+++ Karty Pracy cpp/KP_6_2.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <cmath>
 using namespace std;
 int main()
 {
@@ -41,6 +42,45 @@ int main()
     //    if (n == 0) {
     //        break;
     //    }
+    //}
+    //cout << suma;
+
+    //Zad.8
+    //int suma = 0;
+    //int x = 2;
+    //int n;
+    //cout << "Podaj liczbe: ";
+    //cin >> n;
+    //for (int i = 0; i < n; i++) {
+    //    if (i % 2 == 0) suma = suma + x;
+    //    else suma = suma - x;
+    //    x += 3;
+    //}
+    //cout << suma;
+
+    //Zad.9
+    //int iloczyn = 1;
+    //int n;
+    //cout << "Podaj liczbe: ";
+    //cin >> n;
+    //for (int i = 0; i < n; i++) {
+    //    if (i % 2 == 0) iloczyn *= pow(2, i);
+    //    else iloczyn *= (-1 * pow(2, i));
+    //}
+    //cout << iloczyn;
+
+    //Zad.10
+    //int suma = 0;
+    //int n;
+    //int x = 1;
+    //cout << "Podaj liczbe: ";
+    //cin >> n;
+    //for (int i = 1; i <= n; i++) {
+    //    for (int j = 1; j <= i; j++) {
+    //        x = x * j;
+    //    }
+    //    suma = suma + x;
+    //    x = 1;
     //}
     //cout << suma;
 }
