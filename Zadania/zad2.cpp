e522246ee111b09fb809ad3bae973d0e4ba16ef9
Marta Mendel <114557943+Qniara@users.noreply.github.com>
2024-03-07 19:04:46 +0100

    Update zad2.cpp

--- Zadania/zad2.cpp
+++ Zadania/zad2.cpp
@@ -1,6 +1,26 @@
 #include <iostream>
 using namespace std;
+bool f(string n) {
+    string n2;
+    for (int i = n.length() - 1; i >= 0; i--) {
+        n2 += n[i];
+    }
+    for (int i = 0; i < n.length(); i++) {
+        if (n[i] != n2[i]) {
+            return false;
+        }
+    }
+    return true;
+}
 int main()
 {
-    
+    string napis;
+    cout << "Podaj napis: ";
+    cin >> napis;
+    if (f(napis) == true) {
+        cout << "Podany wyraz jest palindoromem.";
+    }
+    else {
+        cout << "Podany wyraz nie jest palindoromem.";
+    }
 }
